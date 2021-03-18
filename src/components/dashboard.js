01a3173d27f316d9accf2578f98bba35c92921c1
mattrossman <rossman@gatech.edu>
2021-03-18 18:44:07 -0400

    Create image asset outside of dashboard component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,37 +1,25 @@
 AFRAME.registerComponent('dashboard', {
   init: function () {
-    // Asset setup
-    if (!document.querySelector('#a-dashboard')) {
-      const assets = document.querySelector('a-assets')
-      const img = document.createElement('img')
-      assets.appendChild(img)
-      img.src = 'https://aelatgt.net/air-quality-dashboard'
-      img.id = 'a-dashboard'
-      img.crossOrigin = 'anonymous'
-    }
-
-    // Placeholder for auto scaling
-    this.el.setAttribute('geometry', null)
-    this.el.setAttribute('material', { visible: false })
-
+    const { naturalWidth, naturalHeight } = img
+    const aspect = naturalWidth / naturalHeight
+    this.el.setAttribute('geometry', {
+      primitive: 'plane',
+      width: aspect,
+      height: 1,
+    })
     this.el.setAttribute('material', {
-      src: '#a-dashboard',
       shader: 'flat',
       side: 'double',
-    })
-
-    /**
-     * When the dashboard image loads, size the plane to the correct aspect
-     * and make it visible
-     */
-    this.el.addEventListener('materialtextureloaded', (e) => {
-      const { naturalWidth, naturalHeight } = e.detail.texture.image
-      const aspect = naturalWidth / naturalHeight
-      this.el.setAttribute('geometry', {
-        primitive: 'plane',
-        height: 1 / aspect,
-      })
-      this.el.setAttribute('material', { visible: true })
+      src: img,
     })
   },
 })
+
+/**
+ * We have to use the asset management system or the image
+ * won't load properly for pinned objects
+ */
+const img = document.createElement('img')
+img.src = 'https://aelatgt.net/air-quality-dashboard'
+img.crossOrigin = 'anonymous'
+document.querySelector('a-assets').appendChild(img)
